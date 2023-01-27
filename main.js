a51a9c9c0dc9e17c6b5e7754fc7acd0caf13f48b
Alexandre Lecoeur <alexandre.lecoeur@gmail.com>
2023-01-27 10:09:32 +0100

    exo done

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ const car = new Car ("Renault", "Kangoo", "240.000km", 2003, "Climatisée" );
 const moto = new Moto ("BMW", "R1150R Rockster", "65.000km", 2005);
 console.log(moto)
 console.log(car.display())
+
+let resultOutput = document.querySelector("#display");
+resultOutput.innerHTML = car.display();
+resultOutput.innerHTML += moto.display();
